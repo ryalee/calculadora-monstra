1d88894e3cd5803967c87b4dec195d65efa20a1f
Ryan <98854650+ryalee@users.noreply.github.com>
2022-12-16 10:59:36 -0300

    Initial commit

---
+++ index-js.js
@@ -0,0 +1,24 @@
+
+function insert(num) {
+    var numero = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = numero + num;
+}
+
+function clean() {
+    document.getElementById('display').innerHTML = ''; 
+}
+
+function back() {
+    var display = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = display.substring(0, display.length -1);
+}
+
+function calcular() {
+    var display = document.getElementById('display').innerHTML;
+
+    if (display) {
+        document.getElementById('display').innerHTML = eval(display)
+    } else {
+        document.getElementById('display').innerHTML = '...'
+    }
+}
